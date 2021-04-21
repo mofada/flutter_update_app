b8d228a41e9e08c621d8a315e1d6223e36659b1f
fada <fada@mofada.cn>
2021-04-21 22:51:30 +0800

    1.Merge pull request #9 2.Update version number, ready to release...

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,7 +16,7 @@ class _MyAppState extends State<MyApp> {
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext? context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
